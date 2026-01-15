f5e2eabccb7c3913404085638d40d99922d7b566
LeeJin0801 <102401077+LeeJin0801@users.noreply.github.com>
2026-01-15 10:58:10 +0900

    [BOJ] 벌집 / 브론즈 2

--- JinLee/BOJ/Bronze/벌집.java
+++ JinLee/BOJ/Bronze/벌집.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
         int layer = 1;
         int range_max = 1;
         while(range_max < n){
