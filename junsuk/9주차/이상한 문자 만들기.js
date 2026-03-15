fc3b7214cf74bfceadad8f6518daa89c789f50e2
JS-Yang <117419920+YJunSuk@users.noreply.github.com>
2026-03-15 15:48:15 +0900

    Create 이상한 문자 만들기.js

---
+++ junsuk/9주차/이상한 문자 만들기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    let answer = '';
+    let index = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === ' '){
+            answer += ' ';
+            index = 0;
+        }else{
+            answer += (index % 2 === 0) ? s[i].toUpperCase() : s[i].toLowerCase();
+            index++;
+        }
+    }
+    return answer;
+}
