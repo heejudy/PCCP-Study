fe20215136331b6c4b239a3271cc26f5ee3792b9
jin605 <jinddd3@gmail.com>
2026-04-09 09:02:43 +0900

    12주차

---
+++ jinhyuk/13주차/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+
+        for (int i = 0; i < t; i++) {
+
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) {
+
+                que.offer(x);
+
+            } else {
+
+                if (que.isEmpty()) {
+
+                    sb.append(0).append("\n");
+                } else {
+
+                    sb.append(que.poll()).append("\n");
+
+                }
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
