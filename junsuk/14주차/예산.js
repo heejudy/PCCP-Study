fae0cdc788e45cecbdb4f136be52aa29b6d3d0bb
JS-Yang <117419920+YJunSuk@users.noreply.github.com>
2026-04-15 21:08:22 +0900

    14주차

---
+++ junsuk/14주차/예산.js
@@ -0,0 +1,17 @@
+function solution(d, budget) {
+    let answer = 0;
+    let sum = 0;
+
+    d.sort((a, b) => a - b);
+
+    for (let i = 0; i < d.length; i++) {
+        if (sum + d[i] <= budget) {
+            sum += d[i];
+            answer++;
+        } else {
+            break;
+        }
+    }
+
+    return answer;
+}
